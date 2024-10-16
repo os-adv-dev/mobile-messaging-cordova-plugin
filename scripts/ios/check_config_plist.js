603c96d6e04aa666b1ba0fe8328fd37bedf63ec5
André Grillo <andregrillo@gmail.com>
2024-10-16 16:47:40 +0100

    projectFile.js fix

--- scripts/ios/check_config_plist.js
+++ scripts/ios/check_config_plist.js
@@ -47,7 +47,7 @@ module.exports = function (context) {
                 console.log("👉 project_dir: " + project_dir);
                 console.log("👉 pbxPath: " + pbxPath);
                 console.log("👉 xcBuildConfiguration: " + JSON.stringify(xcBuildConfiguration));
-                console.log("👉 plist_file_entry: " + plist_file_entry);
+                console.log("👉 plist_file_entry: " + JSON.stringify(plist_file_entry));
                 console.log("👉 plist_file: " + plist_file);
                 console.log("👉 config_file: " + config_file);
             `;
