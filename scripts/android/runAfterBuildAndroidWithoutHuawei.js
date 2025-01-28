0df98745707a0d16d3d3edeb6199b83508c5ee6b
André Grillo <34004426+andregrillo@users.noreply.github.com>
2025-01-28 16:15:26 +0000

    Update runAfterBuildAndroidWithoutHuawei.js

--- scripts/android/runAfterBuildAndroidWithoutHuawei.js
+++ scripts/android/runAfterBuildAndroidWithoutHuawei.js
@@ -80,6 +80,7 @@ module.exports = function(context) {
 function runAfterBuildHook(context) {
     const deferred = Q.defer();
     console.log('✅ -- RUN BUILD APP WITHOUT HUAWEI NORMAL APK TO USE IN QR CODE --');
+    console.log("👉 Cordova Version: " + cordova.version);
     
     const isDebug = context.cmdLine.includes('debug');
     const projectRoot = context.opts.projectRoot;
