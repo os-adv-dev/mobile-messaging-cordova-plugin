93d6048b9bfbea621172feccafb15ee1e2753cac
agoncalvesos <andre.goncalves@outsystems.com>
2026-06-22 21:59:52 +0100

    Update integrate_notification_extension.js

--- scripts/integrate_notification_extension.js
+++ scripts/integrate_notification_extension.js
@@ -451,7 +451,7 @@ function modifyPodfile(podfilePath, projectName, mmVersion) {
             "  installer.pods_project.targets.each do |target|\n" +
             "    if target.name == 'MobileMessaging' || target.name == 'MobileMessagingNotificationExtension'\n" +
             "      target.build_configurations.each do |config|\n" +
-        "        config.build_settings['SWIFT_VERSION'] = '5.7'\n" +
+        "        config.build_settings['SWIFT_VERSION'] = '6'\n" +
             "      end\n" +
             "    end\n" +
             "  end\n" +
